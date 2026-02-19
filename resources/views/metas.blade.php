74c47f5485f1f347f5903e15034c710183893a0b
Julian David Lizcano Manrique <jlizcano432@unab.edu.co>
2026-02-18 20:20:53 -0500

    Vistas metas.blade.php creada

---
+++ resources/views/metas.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Metas</title>
+    <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+</head>
+<body>
+
+<h1>Metas Profesionales</h1>
+
+<p>
+Mi principal meta es culminar exitosamente la carrera de Ingeniería en Sistemas,
+construyendo una base sólida en desarrollo de software y arquitectura de sistemas.
+</p>
+
+<p>
+Aspiro a desempeñarme como desarrollador profesional, participando en proyectos
+que aporten soluciones tecnológicas eficientes y de calidad.
+</p>
+
+<p>
+A largo plazo, busco especializarme en tecnologías modernas y mantener una
+formación continua que me permita adaptarme a los cambios del sector tecnológico.
+</p>
+
+<nav>
+    <a href="{{route('perfil')}}">Perfil</a> |
+    <a href="{{route('intereses')}}">Intereses</a> |
+    <a href="{{route('habilidades')}}">Habilidades</a>
+</nav>
+
+</body>
+</html>
