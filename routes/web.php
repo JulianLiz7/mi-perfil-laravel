e290070b2936659253f0fee332b43881540d6ff7
Julian David Lizcano Manrique <jlizcano432@unab.edu.co>
2026-02-18 20:15:20 -0500

    Vista perfil.blade.php creada

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('welcome'); 
+})->name('welcome');
 
 Route::get('/perfil', function () {
     return view('perfil');
-});
+})->name('perfil');
 
 Route::get('/perfil/intereses', function () {
     return view('intereses');
-});
+})->name('intereses');
 
 Route::get('/perfil/habilidades', function () {
     return view('habilidades');
-});
+})->name('habilidades');
 
 Route::get('/perfil/metas', function () {
     return view('metas');
-});
+})->name('metas');
+
